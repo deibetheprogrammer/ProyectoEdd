bff268b5e20480b9c0d96613c28da235f995a22d
David Bendeck <45319861+deibetheprogrammer@users.noreply.github.com>
2020-08-02 18:52:02 -0600

    primer commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoEdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
